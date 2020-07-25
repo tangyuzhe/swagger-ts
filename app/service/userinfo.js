681a1cff19e69b4309f5279d6363b8e483818b94
tangyuzhe <www.twuwu@qq.com>
2020-07-25 15:04:56 +0800

    add wxlogin

---
+++ app/service/userinfo.js
@@ -0,0 +1,23 @@
+'use strict';
+const Service = require('egg').Service;
+const qs = require('qs');
+class UserInfoService extends Service {
+  async getWXAuth(Code) {
+    const { ctx } = this;
+    const obj = {
+      appid: 'wx3508ae3ee71e9f68',
+      secret: '0bbec777652406507af7c0516d82dbb1',
+      code: Code,
+      grant_type: 'authorization_code',
+    };
+    const res = await ctx.curl('https://api.weixin.qq.com/sns/oauth2/access_token?' + qs.stringify(obj));
+    return JSON.parse(res.data.toString());
+  }
+
+  async getInfo(access_token, openid) {
+    const { ctx } = this;
+    const res = await ctx.curl('https://api.weixin.qq.com/sns/userinfo?access_token=' + access_token + '&openid=' + openid + '&lang=zh_CN');
+    return JSON.parse(res.data.toString());
+  }
+}
+module.exports = UserInfoService;
