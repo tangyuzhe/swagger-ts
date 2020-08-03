d23a8331759fa43ef5ac1436cd2c2a573601d373
tangyuzhe <38202539+tangyuzhe@users.noreply.github.com>
2020-08-03 12:48:51 +0800

    积分的请求添加

--- app/controller/Integrals.js
+++ app/controller/Integrals.js
@@ -6,32 +6,42 @@ const Controller = require('egg').Controller;
 */
 class IntegralsController extends Controller {
   /**
-   * @summary 创建个人积分
+   * @summary 修改个人积分
    * @description 每次做完题提交成绩获取积分
    * @router put /api/integrals
-   * @request query string name
    * @request query string openid
    * @request query double integrals
    * @response 200 Integrals 查询成功
    */
-  async create() {
+  async update() {
     const { ctx, service } = this;
-    const name = ctx.query.name;
     const openid = ctx.query.openid;
     const integrals = ctx.query.integrals;
-    ctx.body = await service.integrals.update(name, openid, integrals);
+    ctx.body = await service.integrals.UpdateIntegral(openid, integrals);
   }
 
   /**
    * @summary 查询个人积分
    * @description 查询个人积分
-   * @router get /api/integrals/{name}
-   * @request path string name
+   * @router get /api/integrals
+   * @request query string openid
    * @response 200 Integrals 查询成功
    */
   async queryIntegral() {
     const { ctx, service } = this;
-    ctx.body = await service.integrals.queryIntegral(ctx.params.name);
+    ctx.body = await service.integrals.queryIntegral(ctx.query.openid);
+  }
+
+  /**
+     * @summary 添加积分
+     * @description 往试题库添加一道题目
+     * @router post /api/integrals
+     * @request body Integrals *body
+     * @response 200 Integrals 创建成功
+     */
+  async create() {
+    const { ctx, service } = this;
+    ctx.body = await service.integrals.create(ctx.request.body)
   }
 }
 
--- app/service/integrals.js
+++ app/service/integrals.js
@@ -3,56 +3,44 @@ const Service = require('egg').Service;
 const Code = require('../util/code');
 class IntegralsService extends Service {
   /**
-   * 修改个人积分
-   * @param Name
-   * @param Openid
-   * @param Integrals
-  */
-  async update(Name, Openid, Integrals) {
+   * 已存在积分进行修改
+   * @param {*} Openid 
+   * @param {*} Integral 
+   */
+  async UpdateIntegral(Openid, Integral) {
     const { ctx } = this;
-    const data = await ctx.model.Integrals.findOne({
-      where: {
-        name: Name,
-      },
+    const res = await ctx.model.Integrals.update(
+      { integrals: Integral },
+      { where: { openid: Openid } }
+    )
+    return Object.assign({}, {
+      data: res,
     });
-    const query = {};
-    if (Openid) {
-      query.openid = Openid;
-    }
-    if (Integrals) {
-      query.integrals = Integrals;
-    }
-    if (!data) {
-      return Object.assign({}, Code.Find.ERROE);
-    }
-    if (data) {
-      const res = await ctx.model.Integrals.update(query, {
-        where: {
-          name: Name,
-        },
-      });
-      if (!res) {
-        return Object.assign({}, Code.Update.ERROE);
-      }
-      if (res) {
-        return Object.assign({}, Code.Update.SUCCESS, {
-          data: res,
-        });
-      }
-    }
+  }
+
+  /**
+   * 添加积分
+   * @param {*} integral 
+   */
+  async create(integral) {
+    const { ctx } = this;
+    const res = await ctx.model.Integrals.create(integral)
+    return Object.assign({}, Code.Add.SUCCESS, {
+      data: res,
+    });
   }
 
 
   /**
    * 查询个人积分
-   * @param Name
+   * @param Openid
    */
-  async queryIntegral(Name) {
+  async queryIntegral(Openid) {
     const { ctx } = this;
     const res = await ctx.model.Integrals.findOne({
       attributes: ['integrals'],
       where: {
-        name: Name,
+        openid: Openid,
       },
     });
     if (!res) {
